a01e8010e053b76b9c00ed883c80b875f1288b3f
Lucas Borges <jlucas19@hotmail.com>
2017-03-02 22:27:03 -0300

    paginando

--- index.php
+++ index.php
@@ -115,23 +115,13 @@
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Dashboard
-        <small>Control panel</small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Dashboard</li>
-      </ol>
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-
-    </section>
-    <!-- /.content -->
+    <?php
+    if(!isset($_GET["page"]) || $_GET["page"] == ""){
+        include_once("home.php");
+    } else if ($_GET["page"] == "prontuarios") {
+        include_once("prontuarios.php");
+    }
+    ?>
   </div>
   <!-- /.content-wrapper -->
   <footer class="main-footer">
